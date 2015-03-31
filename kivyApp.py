e689d0814cad21be6316abb5195204f25786624f
Xynnect <dido.jordanov7@gmail.com>
2015-03-31 02:50:58 +0100

    trying to use the kivy framework as a wrapper for android

--- kivyApp.py
+++ kivyApp.py
@@ -1,1 +1,15 @@
-__author__ = 'zyan'
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyPaintWidget(Widget):
+    pass
+
+
+class MyPaintApp(App):
+    def build(self):
+        return MyPaintWidget()
+
+
+if __name__ == '__main__':
+    MyPaintApp().run()
