b0a170c54d1a9015e91c6d5e06f5d611e971cafe
Xynnect <dido.jordanov7@gmail.com>
2015-03-31 04:14:14 +0100

    tried but failed to clone from git for the buildozer

--- kivyApp.py
+++ kivyApp.py
@@ -1,12 +1,19 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse
 
 
 class MyPaintWidget(Widget):
-    pass
+
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 
 
 class MyPaintApp(App):
+
     def build(self):
         return MyPaintWidget()
 
