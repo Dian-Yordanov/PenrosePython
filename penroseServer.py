18e312cb121bf7be557dfc69c77130caa804dd77
Xynnect <dido.jordanov7@gmail.com>
2015-04-07 05:08:23 +0100

    did the python server

--- penroseServer.py
+++ penroseServer.py
@@ -106,3 +106,15 @@ print(uploaded_image.size)
 print(uploaded_image.type)
 print(uploaded_image.id)
 print(uploaded_image.name)
+
+from twilio.rest import TwilioRestClient
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC53d28f419faa0e6571046b8db0629e0b"
+auth_token  = "a95329d734fcda0a20da363743dd37bc"
+client = TwilioRestClient(account_sid, auth_token)
+
+sms = client.sms.messages.create(body=uploaded_image.link,
+    to="+4407479265143",
+    from_="+441163262195")
+print sms.sid
