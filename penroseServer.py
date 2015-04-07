b9c4ba95288ce977e4c8fdd5d350c7caa87631c6
Xynnect <dido.jordanov7@gmail.com>
2015-04-07 04:08:48 +0100

    updated the server to not only draw the image but also sent it to Imgur

--- penroseServer.py
+++ penroseServer.py
@@ -91,3 +91,18 @@ cr.stroke()
 
 # Save to PNG
 surface.write_to_png('penrose.png')
+
+import pyimgur
+
+CLIENT_ID = "8e6cf031726addc"
+PATH = "/home/zyan/PycharmProjects/PenrosePython/penrose.png"
+
+im = pyimgur.Imgur(CLIENT_ID)
+uploaded_image = im.upload_image(PATH, title="Uploaded11")
+gallery_image = im.create_album(title="penrosetill",images=uploaded_image.id)
+print(uploaded_image.title)
+print(uploaded_image.link)
+print(uploaded_image.size)
+print(uploaded_image.type)
+print(uploaded_image.id)
+print(uploaded_image.name)
